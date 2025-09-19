27842a2ca7218f27997bddbd39f53ff39f848bdd
RICK <rique88858@gmail.com>
2025-09-19 09:39:36 -0300

    Update SaleInterface.ts

--- src/interfaces/SaleInterface.ts
+++ src/interfaces/SaleInterface.ts
@@ -2,9 +2,8 @@ export interface Sale {
   id: string;
   nomeProduto: string;
   quantidadeProduto: number;
-  metodoPagamento: string;
-  statusPagamento: string;
+
   precoVenda: number;
   precoCusto: number;
-  data: Date;
+
 }
